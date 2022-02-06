90b246a6a7b6c8bf0c564f9493f01643efe91534
saurav2502 <kumar_sauravsk@outlook.com>
2022-02-06 08:40:02 +0530

    golang yaml file config read added

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ echo "Building $APP_NAME ..."
 APP_VERSION=v2
 APP_TAGS=$APP_NAME:$APP_VERSION
 
-docker build -t $APP_TAGS .
+docker build -t "saurav2502/$APP_TAGS" .
 
 docker run -d \
 -p 8088:8083 \
---name $APP_TAGS \
+--name "$APP_NAME"_"$APP_APP_VERSION" \
 --network=go-net \
 -v go-app:/usr/share/awesomeProject \
-$APP_TAGS
+"saurav2502/$APP_TAGS"
 
 echo "Building finished successfully"
--- route/route.go
+++ route/route.go
@@ -1,11 +1,15 @@
 package route
 
 import (
+	"awesomeProject/config"
 	"awesomeProject/service"
 	_ "awesomeProject/service"
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type App struct {
@@ -13,7 +17,36 @@ type App struct {
 }
 
 func (app *App) Run() {
-	log.Fatal(http.ListenAndServe(":8083", app.Router))
+	// Set the file name of the configurations file
+	viper.SetConfigName("config")
+
+	// Set the path to look for the configurations file
+	viper.AddConfigPath(".")
+
+	// Enable VIPER to read Environment Variables
+	viper.AutomaticEnv()
+
+	viper.SetConfigType("yml")
+	var configuration config.Configurations
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+	}
+
+	// Set undefined variables
+	viper.SetDefault("database.dbname", "test_db")
+
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+
+	// Reading variables using the model
+	fmt.Println("Port is\t\t", configuration.Server.Port)
+	fmt.Println("DBURL is\t\t", configuration.Database.DbUrl)
+	port := ":" + strconv.Itoa(configuration.Server.Port)
+	log.Print("App is running on the port ", port)
+	log.Fatal(http.ListenAndServe(port, app.Router))
 }
 
 func (app *App) InitialiseRoutes() {
